20d6fae9a0b365c6b8777f242c403bb0004eace9
Qubert <upafourth@gmail.com>
2017-11-25 16:19:03 +0100

    Add project files.

---
+++ CTCI 1.4 Replace Spaces/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTCI_1._4_Replace_Spaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
